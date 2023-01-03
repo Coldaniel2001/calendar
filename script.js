0084fa004cc3f5159cb7a5f2f84bbc4d7a0ad81e
Daniel Wilson <danielvillenawilson@gmail.com>
2023-01-03 15:57:32 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const calendarMonth = document.getElementById("calendar-month");
+const calendar = document.getElementById("calendar-month");
+const calendarMonth = document.getElementsByClassName("month")[0];
 function printDays() {
     const date = new Date();
     console.log(date);
+    let getMonthArray = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+    calendarMonth.textContent = getMonthArray[date.getMonth()];
+
     let numberDays = new Date(
       date.getFullYear(),
       date.getMonth() + 1,
@@ -12,7 +17,7 @@ function printDays() {
         day.classList.add("day");
         const dayNumber = document.createElement('p');
         dayNumber.textContent = i+1;
-        calendarMonth.appendChild(day);
+        calendar.appendChild(day);
         day.appendChild(dayNumber);
         
     }
