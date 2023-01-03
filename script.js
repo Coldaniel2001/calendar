ba4a7a7008093f80a585f93466293edb4434e0c0
Miquel <abellagarciamiquel@gmail.com>
2023-01-03 16:29:08 +0100

    month navigation done

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
 const calendar = document.getElementById("calendar-month");
 const calendarMonth = document.getElementsByClassName("month")[0];
-function printDays() {
-    const date = new Date();
-    console.log(date);
-    let getMonthArray = ["January","February","March","April","May","June","July",
-    "August","September","October","November","December"];
-    calendarMonth.textContent = getMonthArray[date.getMonth()];
 
-    let numberDays = new Date(
-      date.getFullYear(),
-      date.getMonth() + 1,
-      0
-    ).getDate();
-    for(let i = 0; i < numberDays; i++){
-        const day = document.createElement('div');
-        day.classList.add("day");
-        const dayNumber = document.createElement('p');
-        dayNumber.textContent = i+1;
-        calendar.appendChild(day);
-        day.appendChild(dayNumber);
-        
-    }
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+
+nextBtn.addEventListener("click", printNextMonth);
+prevBtn.addEventListener("click", printPrevMonth);
+
+let monthCount = 0;
+let yearCount = 0;
+
+document.body.addEventListener("load", printDays(monthCount, yearCount));
+
+function printDays(monthCounter, yearCounter) {
+  calendar.innerHTML = "";
+
+  const date = new Date();
+
+  let getMonthArray = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  calendarMonth.textContent =
+    getMonthArray[date.getMonth() + monthCounter] + ' ' +
+    (parseInt(date.getFullYear()) +
+    yearCounter);
+
+  let numberDays = new Date(
+    date.getFullYear() + yearCounter,
+    date.getMonth() + 1 + monthCounter,
+    0
+  ).getDate();
+  for (let i = 0; i < numberDays; i++) {
+    const day = document.createElement("div");
+    day.classList.add("day");
+    const dayNumber = document.createElement("p");
+    dayNumber.textContent = i + 1;
+    calendar.appendChild(day);
+    day.appendChild(dayNumber);
   }
-  
-printDays();
-  
+}
+
+function printNextMonth() {
+  monthCount++;
+
+  if (monthCount === 12) {
+    monthCount = 0;
+    yearCount++;
+  }
+
+  printDays(monthCount, yearCount);
+}
+
+function printPrevMonth() {
+  monthCount--;
+
+  if (monthCount === -1) {
+    monthCount = 11;
+    yearCount--;
+  }
+
+  printDays(monthCount, yearCount);
+}
